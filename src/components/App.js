bc17a7bb1e72a4e44bc9ef38b31612f6601ec1c4
DominNumb <dominik.numb@gmail.com>
2022-04-23 22:27:03 +0200

    Editnul jsem login label

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import './App.css'
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Please LogIn</h1>
       <input value="" placeholder="Username" />
       <input type="password" value="" placeholder="Password" />
       <button>Login</button>
