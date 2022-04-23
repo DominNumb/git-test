617ad8f4fa0af8e1b4c711212cdab7acf3cefba7
DominNumb <dominik.numb@gmail.com>
2022-04-23 22:09:27 +0200

    Pridal som password input

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <h1>Hello</h1>
       <input value="" placeholder="Username" />
+      <input type="password" value="" placeholder="Password" />
     </div>
   )
 }
