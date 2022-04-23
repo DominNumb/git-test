b8810d7622fdf10c3d316fb76bd81261d757996e
DominNumb <dominik.numb@gmail.com>
2022-04-23 22:16:33 +0200

    Pridal som login button

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ function App() {
       <h1>Hello</h1>
       <input value="" placeholder="Username" />
       <input type="password" value="" placeholder="Password" />
+      <button>Login</button>
     </div>
   )
 }
