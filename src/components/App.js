dd6e494bd08d753994fd2b27be13f8259f8e93b8
DominNumb <100333809+DominNumb@users.noreply.github.com>
2022-04-24 00:30:50 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <div style={{ background: 'grey' }}>
-        <h1 style={{ color: 'white' }}>Please LogIn</h1>
+        <h1 style={{ color: 'white' }}>Super nadpis</h1>
         <input value="" placeholder="Username" />
         <br />
         <input type="password" value="" placeholder="Password" />
