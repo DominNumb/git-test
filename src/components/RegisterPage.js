1282ca31290f1a9d2956516cbb56f4a229fa588f
DominNumb <dominik.numb@gmail.com>
2022-04-24 14:42:46 +0200

    Head text added to Register and Login page

---
+++ src/components/RegisterPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+class RegisterPage extends Component {
+  render() {
+    return (
+      <>
+        <div>
+          <h1>Register Page</h1>
+        </div>
+      </>
+    )
+  }
+}
+
+export default RegisterPage
